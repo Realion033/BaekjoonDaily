4f581eb1c08bfd954022255e8e7b136a00bbf0f6
Realion033 <93594604+Realion033@users.noreply.github.com>
2024-05-29 23:51:31 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	int 이게뭐노;
+	
+
+	cin >> 이게뭐노;
+
+	for (int i = 0; i < 이게뭐노; i++) {
+		string 이건뭐야;
+		cin >> 이건뭐야;
+		cout << 이건뭐야.front() << 이건뭐야.back() << endl;
+	}
+}
