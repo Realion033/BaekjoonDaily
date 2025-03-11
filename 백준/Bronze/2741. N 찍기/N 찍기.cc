2a5da346f2da0c13d74585fab207660fb2906d70
Realion033 <93594604+Realion033@users.noreply.github.com>
2025-03-11 19:09:09 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << '\n'; // endl 대신 '\n' 사용
+    }
+    return 0;
+}
